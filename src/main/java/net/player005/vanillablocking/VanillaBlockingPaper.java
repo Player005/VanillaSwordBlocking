baad367b38b5b31b15106f380f1952559c0636da
player005 <me@player005.net>
2024-11-03 14:13:39 +0100

    refactor and fixes

--- src/main/java/net/player005/vanillablocking/VanillaBlockingPaper.java
+++ src/main/java/net/player005/vanillablocking/VanillaBlockingPaper.java
@@ -7,7 +7,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -19,25 +21,42 @@ public class VanillaBlockingPaper extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @EventHandler
-    public void onInventoryUpdate(@NotNull InventoryClickEvent event) {
-        if (event.getClickedInventory() == null) return;
-        boolean isPlayer = event.getClickedInventory().getHolder() instanceof Player;
-        for (@Nullable ItemStack stack : event.getClickedInventory().getContents()) {
+    /**
+     * Updates the necessary components on all swords in an inventory
+     *
+     * @param inventory the inventory
+     * @param add       whether to add or remove the component that allows blocking the sword
+     */
+    private static void updateAllItems(@NotNull Inventory inventory, boolean add) {
+        for (@Nullable ItemStack stack : inventory.getContents()) {
             if (stack == null) continue;
-            if (isPlayer) VanillaBlocking.addSwordComponents(((CraftItemStack) stack).handle);
+            if (add) VanillaBlocking.addSwordComponents(((CraftItemStack) stack).handle);
             else VanillaBlocking.removeSwordComponents(((CraftItemStack) stack).handle);
         }
     }
 
+    // when items transferred to player inventory, make swords blockable
+    // when items transferred to different inventory, remove component
     @EventHandler
-    public void onDisconnect(@NotNull PlayerQuitEvent event) {
-        for (@Nullable ItemStack stack : event.getPlayer().getInventory().getContents()) {
-            if (stack == null) continue;
-            VanillaBlocking.removeSwordComponents(((CraftItemStack) stack).handle);
-        }
+    public void onInventoryUpdate(@NotNull InventoryClickEvent event) {
+        if (event.getClickedInventory() == null) return;
+        boolean isPlayerInventory = event.getClickedInventory().getHolder() instanceof Player;
+        updateAllItems(event.getClickedInventory(), isPlayerInventory);
     }
 
+    // When player joins, make all their swords blockable
+    @EventHandler
+    public void onJoin(@NotNull PlayerJoinEvent event) {
+        updateAllItems(event.getPlayer().getInventory(), true);
+    }
+
+    // When player leaves, remove blockable components from swords
+    @EventHandler
+    public void onDisconnect(@NotNull PlayerQuitEvent event) {
+        updateAllItems(event.getPlayer().getInventory(), false);
+    }
+
+    // reduce damage by 50% when sword is blocked
     @EventHandler
     public void onDamagePlayer(@NotNull EntityDamageEvent event) {
         if (event.getEntity() instanceof Player player) {
