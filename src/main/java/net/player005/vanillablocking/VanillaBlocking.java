95b904671556ececffa38a444ca467cb1bd90eb4
player005 <me@player005.net>
2025-02-15 19:40:58 +0100

    fix possible npe

--- src/main/java/net/player005/vanillablocking/VanillaBlocking.java
+++ src/main/java/net/player005/vanillablocking/VanillaBlocking.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUseAnimation;
 import net.minecraft.world.item.component.Consumable;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public final class VanillaBlocking {
 
@@ -21,7 +22,8 @@ public final class VanillaBlocking {
         return player.getUseItem().is(ItemTags.SWORDS);
     }
 
-    public static void addSwordComponents(@NotNull ItemStack itemStack) {
+    public static void addSwordComponents(@Nullable ItemStack itemStack) {
+        if (itemStack == null) return;
         if (!itemStack.is(ItemTags.SWORDS)) return;
         if (itemStack.getComponents().has(DataComponents.CONSUMABLE)) return;
         itemStack.applyComponents(
